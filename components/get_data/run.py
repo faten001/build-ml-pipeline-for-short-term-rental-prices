b336ccc75962793f1fd08c282441964cd81289a6
faten naief <80006052+faten001@users.noreply.github.com>
2024-10-11 21:03:03 +0300

    Update run.py

--- components/get_data/run.py
+++ components/get_data/run.py
@@ -28,8 +28,9 @@ def go(args):
     )
     artifact.add_file(os.path.join("data", args.sample))
     artifact.description = args.artifact_description
-    artifact.aliases=['latest','reference']
     artifact.wait()
+    artifact.aliases=['latest','reference']
+    
     run.log_artifact(artifact)
 
 if __name__ == "__main__":

d6155a42bf47ca649b4a12c063efb766bf69b400
faten naief <80006052+faten001@users.noreply.github.com>
2024-10-11 15:09:35 +0300

    Update run.py

--- components/get_data/run.py
+++ components/get_data/run.py
@@ -8,7 +8,7 @@ import os
 
 import wandb
 
-from wandb.log_artifact import log_artifact
+#from wandb.log_artifact import log_artifact
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -21,12 +21,12 @@ def go(args):
 
     logger.info(f"Returning sample {args.sample}")
     logger.info(f"Uploading {args.artifact_name} to Weights & Biases")
-    log_artifact(
+    run.log_artifact(
         args.artifact_name,
         args.artifact_type,
         args.artifact_description,
         os.path.join("data", args.sample),
-        run,
+      
     )
 
 
